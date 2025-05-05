bec9ac14f4cc31be876d79871467976abdcdccfb
Vijay Hardaha <vijayhardaha@gmail.com>
2025-05-05 14:23:34 +0530

    fix: remove unused head section from RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,12 +52,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      </head>
       <body
         className={`${customSans.variable} ${customMono.variable} ${customHeading.variable} text-foreground bg-background font-base font-sans font-normal antialiased`}
       >
