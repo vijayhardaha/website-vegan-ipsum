30c06a997c22b9eb31f3d43976de1fc2faab4004
Vijay Hardaha <vijayhardaha@gmail.com>
2025-05-05 11:00:17 +0530

    chore: add layout component with header, main, and footer

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode } from "react";
+
+import Footer from "@/components/footer/Footer";
+import Header from "@/components/header/Header";
+import Main from "@/components/layout/Main";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Header />
+      <Main>{children}</Main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
