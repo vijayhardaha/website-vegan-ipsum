15a065bab43b8748494cce969ac6fab3d4539b2a
Vijay Hardaha <vijayhardaha@gmail.com>
2025-05-21 12:25:16 +0530

    fix: enhance comments in postcss.config.mjs for clarity

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,7 @@
+// PostCSS configuration object
 const config = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: ["@tailwindcss/postcss"], // Specifies the PostCSS plugins to be used, including Tailwind CSS
 };
 
+// Export the configuration object for use by PostCSS
 export default config;
