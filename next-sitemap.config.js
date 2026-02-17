e3b8f1750e0a68a1008626ee23a3c2c3ac20f058
Vijay Hardaha <vijayhardaha@gmail.com>
2026-02-17 18:17:47 +0530

    chore: format configuration files for improved readability

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,20 +1,20 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: "https://veganipsum.vercel.app",
-  generateRobotsTxt: true,
-  sitemapBaseFileName: "sitemap",
-  changefreq: "weekly",
-  priority: 0.7,
-  trailingSlash: false,
-  exclude: ["/404", "/500"],
-  robotsTxtOptions: {
-    policies: [{ userAgent: "*", allow: "/" }],
-    transformRobotsTxt: async (_, robotsTxt) => {
-      const withoutHost = robotsTxt.replace(`# Host\nHost: https://veganipsum.vercel.app\n\n`, "");
+	siteUrl: "https://veganipsum.vercel.app",
+	generateRobotsTxt: true,
+	sitemapBaseFileName: "sitemap",
+	changefreq: "weekly",
+	priority: 0.7,
+	trailingSlash: false,
+	exclude: ["/404", "/500"],
+	robotsTxtOptions: {
+		policies: [{ userAgent: "*", allow: "/" }],
+		transformRobotsTxt: async (_, robotsTxt) => {
+			const withoutHost = robotsTxt.replace(`# Host\nHost: https://veganipsum.vercel.app\n\n`, "");
 
-      return withoutHost;
-    },
-  },
+			return withoutHost;
+		},
+	},
 };
 
 module.exports = config;
--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,6 @@
 // PostCSS configuration object
 const config = {
-  plugins: ["@tailwindcss/postcss"], // Specifies the PostCSS plugins to be used, including Tailwind CSS
+	plugins: ["@tailwindcss/postcss"], // Specifies the PostCSS plugins to be used, including Tailwind CSS
 };
 
 // Export the configuration object for use by PostCSS
