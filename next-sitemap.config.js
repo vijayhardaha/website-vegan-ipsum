5c2c143c141ebd6e3cdd0cb8cd5f413fed038191
Vijay Hardaha <vijayhardaha@proton.me>
2026-02-23 19:03:23 +0530

    refactor: add transform function to sitemap configuration

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -7,6 +7,18 @@ const config = {
 	priority: 0.7,
 	trailingSlash: false,
 	exclude: ["/404", "/500"],
+	/**
+	 * Transform each generated sitemap entry.
+	 * This removes milliseconds from lastmod.
+	 */
+	transform: async (config, path) => {
+		return {
+			loc: path,
+			changefreq: config.changefreq,
+			priority: config.priority,
+			lastmod: new Date().toISOString().replace(/\.\d{3}Z$/, "Z"), // remove milliseconds
+		};
+	},
 	robotsTxtOptions: {
 		policies: [{ userAgent: "*", allow: "/" }],
 		transformRobotsTxt: async (_, robotsTxt) => {
